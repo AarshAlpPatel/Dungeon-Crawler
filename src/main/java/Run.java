ae330a2c594351f160366d80dbb3a80c8a0546d8
ocean-ift <karimiftekherul689@gmail.com>
2021-02-16 23:30:32 -0500

    Made run a JavaFX file

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,4 +1,13 @@
 package main.java;
 
-public class Run {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Run extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+    
 }
