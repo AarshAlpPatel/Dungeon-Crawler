1ed1a0f8f2d9ed8fd89827dcdabde5f7ccf1b485
ocean-ift <karimiftekherul689@gmail.com>
2021-02-25 13:26:51 -0500

    Modified Test

--- src/test/java/StartGameTest.java
+++ src/test/java/StartGameTest.java
@@ -1,25 +1,40 @@
 package test.java;
 
+import javafx.scene.Scene;
 import main.frontend.*;
 
 import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.matcher.base.NodeMatchers;
+import org.testfx.util.WaitForAsyncUtils;
 
 import static org.testfx.api.FxAssert.verifyThat;
 
 public class StartGameTest extends ApplicationTest {
+
+    private Stage stage;
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
+        stage = primaryStage;
         MainScreen mainScreen = new MainScreen();
         mainScreen.start(primaryStage);
     }
 
+//    @BeforeEach
+//    public void setup() {
+//        MainScreen mainScreen = new MainScreen();
+//        mainScreen.start(stage);
+//    }
+
     @Test
     public void testStartGame() {
+//        FxRobot robot = new FxRobot();
         clickOn("Start Game");
+        //WaitForAsyncUtils.waitForFxEvents();
         verifyThat("Enter Name", NodeMatchers.isNotNull());
     }
 
