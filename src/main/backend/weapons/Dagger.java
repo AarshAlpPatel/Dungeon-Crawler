32170c096356e762fb578af140761ab102553b0a
Kevin Zheng <kevinzheng813@gmail.com>
2021-03-19 01:19:55 -0400

    changing how weapons look on a sprite

--- src/main/backend/weapons/Dagger.java
+++ src/main/backend/weapons/Dagger.java
@@ -1,8 +1,8 @@
 package main.backend.weapons;
 
 public class Dagger extends Weapon {
-    public Dagger(double x, double y, int id, boolean dropped) {
-        super(x, y, 0, 10, 5, 45, id, "daggerh.png", dropped, 80, 10);
+    public Dagger(double x, double y, int id, boolean dropped, double translateX, double translateY) {
+        super(x, y, 0, 10, 5, 45, id, "daggerh.png", dropped, 80, 10, translateX, translateY);
     }
 
     @Override
